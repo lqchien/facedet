3b19e0d4e42e0b9d4335a9c023a8406fc9eca41f
lqchien <lqchien@gmail.com>
2024-09-30 01:46:55 +0700

    Update utils/firebase_utils.py

--- app.py
+++ app.py
@@ -33,17 +33,17 @@ if uploaded_file is not None:
         
         # Loop through detected faces, crop and upload each one
         for (x, y, w, h) in faces:
-            face_id = f"{face_id_counter}"
-            face_id_counter += 1
+#            face_id = f"{face_id_counter}"
+#            face_id_counter += 1
 
-            # Crop the face
-            cropped_face = crop_face(image, x, y, w, h)
+#            # Crop the face
+#            cropped_face = crop_face(image, x, y, w, h)
 
-            # Upload the cropped face image to Firebase
-            img_url = upload_image(bucket, cropped_face, face_id)
+#            # Upload the cropped face image to Firebase
+#            img_url = upload_image(bucket, cropped_face, face_id)
 
-            # Store face metadata in Firestore
-            add_face_data(db, face_id, img_url)
+#            # Store face metadata in Firestore
+#            add_face_data(db, face_id, img_url)
 
             # Draw a rectangle around the face on the main image
             cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
