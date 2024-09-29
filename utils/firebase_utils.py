1ebd899d7ce7a2e9a731793395244cc190e68245
lqchien <lqchien@gmail.com>
2024-09-29 23:47:33 +0700

    Update firebase_utils.py

--- utils/firebase_utils.py
+++ utils/firebase_utils.py
@@ -23,9 +23,9 @@ def init_firebase_local():
 # Initialize Firebase Admin SDK
 # https://www.youtube.com/watch?v=qAYqdg9UICc&ab_channel=TechnicalRajni
 def init_firebase_cloud():
-#    firebase_key = json.loads(os.getenv('FIREBASE_KEY'))
-#    cred = credentials.Certificate(firebase_key)
-    cred = credentials.Certificate("firebase/serviceAccountKey.json")
+    firebase_key = json.loads(os.getenv('FIREBASE_KEY'))
+    cred = credentials.Certificate(firebase_key)
+#    cred = credentials.Certificate("firebase/serviceAccountKey.json")
     try:
         firebase_admin.get_app()
     except ValueError:
